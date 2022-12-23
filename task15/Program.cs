daf53d992b99da6b017d65b8f5059635e26b4c18
Ruslan <kirito7379@gmail.com>
2022-12-23 20:34:38 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] a = {1, 2, 3, 4, 5, 6, 7,};
+int b;
+Console.Write($"Введите число ");
+int.TryParse(Console.ReadLine()!, out b);
+if(b == 6 | b == 7)
+{
+    Console.Write($"Это выходной день");
+}
+else
+{
+    Console.Write($"Это не выходной день");
+}
+
+
+
+
+
+
+
+
+
+
