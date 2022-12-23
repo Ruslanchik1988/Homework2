96cbd55259d069464fdd993f18cceae3df45c1df
Ruslan <kirito7379@gmail.com>
2022-12-23 19:18:57 +0300

    task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Write($"Введите число ");
+string a = Console.ReadLine()!;
+if(a.Length > 2 )
+{
+    Console.Write("Третья цифра " + a[2]);
+}
+else
+{
+    Console.Write("Третьей цифры нет ");
+}
+
+
+
+
